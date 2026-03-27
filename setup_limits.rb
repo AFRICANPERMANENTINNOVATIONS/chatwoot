1b15bddbc7bdaf02932178cb144c19f561695aaf
lordkkjmix <lordkkjmix@gmail.com>
2026-03-27 22:55:29 +0000

    fix admin panel branding

--- setup_limits.rb
+++ setup_limits.rb
@@ -2,21 +2,26 @@
 # Usage: bundle exec rails runner setup_limits.rb
 
 configs = {
-  'INSTALLATION_PRICING_PLAN_QUANTITY' => 999999,
-  'ACCOUNT_AGENTS_LIMIT' => 999999,
+  'INSTALLATION_PRICING_PLAN_QUANTITY' => 999_999,
+  'ACCOUNT_AGENTS_LIMIT' => 999_999,
   'INSTALLATION_PRICING_PLAN' => 'enterprise'
 }
 
-configs.each do |name, value|
-  config = InstallationConfig.find_or_create_by(name: name)
-  config.update!(value: value)
-  puts "#{name}: #{value}"
+configs.each do |name, val|
+  config = InstallationConfig.find_by(name: name)
+  if config
+    config.value = val
+    config.save!
+  else
+    InstallationConfig.create!(name: name, value: val)
+  end
+  puts "#{name}: #{val}"
 end
 
 # Update all existing accounts
 Account.find_each do |account|
-  account.update!(limits: { 'agents' => 100_000, 'inboxes' => 100_000 })
+  account.update!(limits: { 'agents' => 999_999, 'inboxes' => 999_999 })
   puts "Account ##{account.id} (#{account.name}): limits updated"
 end
 
-puts "Done - All limits removed"
+puts 'Done - All limits removed'
