c45c9aa37b7533ffde53e47fe3e4995a2c9b5fcd
lordkkjmix <lordkkjmix@gmail.com>
2026-04-02 13:55:33 +0000

    fix: Update bubble icon

--- app/javascript/sdk/bubbleHelpers.js
+++ app/javascript/sdk/bubbleHelpers.js
@@ -7,8 +7,9 @@ import {
 } from '../widget/constants/sdkEvents';
 import { dispatchWindowEvent } from 'shared/helpers/CustomEventHelper';
 
+// PerfectCX custom bubble icon (speech bubble with dot)
 export const bubbleSVG =
-  'M240.808 240.808H122.123C56.6994 240.808 3.45695 187.562 3.45695 122.122C3.45695 56.7031 56.6994 3.45697 122.124 3.45697C187.566 3.45697 240.808 56.7031 240.808 122.122V240.808Z';
+  'M120 20C64.8 20 20 64.8 20 120c0 18.4 5 35.6 13.8 50.4L20 220l49.6-13.8C84.4 215 102.6 220 120 220c55.2 0 100-44.8 100-100S175.2 20 120 20zm0 180c-16.4 0-31.8-4.6-44.8-12.6l-3.2-1.8-33 9.2 9.2-33-1.8-3.2C38.6 151.8 34 136.4 34 120c0-47.6 38.4-86 86-86s86 38.4 86 86-38.4 86-86 86z';
 
 export const body = document.getElementsByTagName('body')[0];
 export const widgetHolder = document.createElement('div');
